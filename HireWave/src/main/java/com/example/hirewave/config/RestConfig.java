920977c087f8bfadd468d66613848f1b28b88882
Bao Tuan <tuannguyen2k4bt@gmail.com>
2026-04-18 17:53:10 +0700

    Merge remote-tracking branch 'origin/main'

--- HireWave/src/main/java/com/example/hirewave/config/RestConfig.java
+++ HireWave/src/main/java/com/example/hirewave/config/RestConfig.java
@@ -1,13 +1,20 @@
-package com.example.hirewave.utility;
+package com.example.hirewave.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig {
+public class RestConfig {
+
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return new ObjectMapper();
     }
 }
